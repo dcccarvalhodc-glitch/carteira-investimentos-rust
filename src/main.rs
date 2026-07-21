b9d3d89be9c574907cec22bea7cba1d3326ec907
dcccarvalhodc-glitch <dcccarvalho.dc@gmail.com>
2026-07-21 18:54:55 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,32 @@
+mod models;
+mod handlers;
 
+fn main() {
+
+    println!("========================================");
+    println!("CARTEIRA DE INVESTIMENTOS");
+    println!("========================================");
+
+    let investimentos = handlers::listar_investimentos();
+
+    println!();
+
+    println!("Investimentos cadastrados:");
+
+    for investimento in investimentos {
+
+        println!("--------------------------------");
+
+        println!("ID: {}", investimento.id);
+
+        println!("Nome: {}", investimento.nome);
+
+        println!("Código: {}", investimento.codigo);
+
+        println!("Quantidade: {}", investimento.quantidade);
+
+        println!("Preço: R$ {:.2}", investimento.preco);
+
+    }
+
+}
